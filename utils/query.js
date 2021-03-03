51e4fb85a198ed6996dfd7bd91d3986d0058e848
Nathan Pham <nathanpham.me@gmail.com>
2021-03-03 08:08:52 -0800

    options in query

---
+++ utils/query.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch")
+const Exception = require("./error")
+
+const parse = res => res.json()
+
+const query = async ({ body, ...options }) => {
+	let headers = require("./headers")
+	try {
+		if(globals.cookies) {
+			headers.Cookie = global.cookies
+		}
+
+		const res = await fetch("https://repl.it/graphql", {
+			method: "POST",
+			headers,
+			body: JSON.stringify(body),
+			...options
+		}).then(parse)
+		
+		return res.data || res.errors || {}
+	}
+	catch(e) {
+		throw new Exception(e)
+	}
+}
+
+module.exports = query
